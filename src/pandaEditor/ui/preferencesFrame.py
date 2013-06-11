d2dce18c21302492fdc1c09b03508845dc09f6a5
derfies <derfies@git>
2013-06-11 22:43:11 +1000

    - Preference frame is now a child of the main process.

--- src/pandaEditor/ui/preferencesFrame.py
+++ src/pandaEditor/ui/preferencesFrame.py
@@ -4,5 +4,5 @@ import wx
 class PreferencesFrame( wx.Frame ):
     
     def __init__( self, *args, **kwargs ):
-        kwargs['style'] = wx.DEFAULT_FRAME_STYLE | wx.FRAME_FLOAT_ON_PARENT
+        kwargs['style'] = wx.DEFAULT_FRAME_STYLE | wx.FRAME_FLOAT_ON_PARENT | wx.FRAME_NO_TASKBAR
         wx.Frame.__init__( self, *args, **kwargs )
